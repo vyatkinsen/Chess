20caaaea07ca5ffcb5000800bd90ea7ef8f3267e
vyatkinsen <vyatkin.aa@edu.spbstu.ru>
2021-05-23 18:41:49 +0300

    cleanup code

--- src/main/java/main/java/sample/MainWindow.java
+++ src/main/java/main/java/sample/MainWindow.java
@@ -31,7 +31,7 @@ public class MainWindow extends Scene {
 
 	public void start(Stage boardStage) {
 		chessBoard = new Board();
-		chessBoard.initPat();
+		chessBoard.initBoard();
 		blackKing = chessBoard.getBlackKing();
 		whiteKing = chessBoard.getWhiteKing();
 
