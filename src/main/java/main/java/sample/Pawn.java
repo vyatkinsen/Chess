fc1af952fcd696914a8fc1d5b824d18ec09fcd00
vyatkinsen <vyatkin.aa@edu.spbstu.ru>
2021-05-23 18:38:50 +0300

    Подкорректированы ходы пешки

--- src/main/java/main/java/sample/Pawn.java
+++ src/main/java/main/java/sample/Pawn.java
@@ -17,11 +17,11 @@ public class Pawn extends Figure {
 			if (this.getFigureColor() == WHITE) offset = -1;
 			else offset = 1;
 
-			if (Math.abs(this.getY() - yPos) == 2 && this.getFigureColor() == BLACK && this.getY() == 1 &&
+			if (Math.abs(this.getY() - yPos) == 2 && this.getFigureColor() == BLACK && this.getY() == 1 && this.getX() == xPos &&
 					board.figureInCell(this.getY() + 1, this.getX()) == null &&
-					board.figureInCell(this.getY() + 2, this.getX()) == null) return true;
+					board.figureInCell(this.getY() + 2, this.getX()) == null ) return true;
 
-			if (Math.abs(this.getY() - yPos) == 2 && this.getFigureColor() == 1 && this.getY() == 6 &&
+			if (Math.abs(this.getY() - yPos) == 2 && this.getFigureColor() == 1 && this.getY() == 6 && this.getX() == xPos &&
 					board.figureInCell(this.getY() - 1, this.getX()) == null &&
 					board.figureInCell(this.getY() - 2, this.getX()) == null) return true;
 
@@ -30,7 +30,7 @@ public class Pawn extends Figure {
 
 			if (Math.abs(this.getY() - yPos) == 1 && Math.abs(this.getX() - xPos) == 1 &&
 					((board.insideBoard(this.getY() + offset, this.getX() + 1) &&
-							board.figureInCell(yPos + offset, this.getX() + 1) != null &&
+							board.figureInCell(this.getY() + offset, this.getX() + 1) != null &&
 					board.figureInCell(this.getY() + offset, this.getX() + 1).getFigureColor() != this.getFigureColor()) ||
 					(this.getY() + offset == board.getyPawnBrokenCell() && this.getX() + 1 == board.getxPawnBrokenCell() &&
 							this.getFigureColor() != board.getColorOfPawnBrokenCell()))) return true;
